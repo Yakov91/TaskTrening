55bc13e1e6fe8804d88b63685dafa1e64d825b93
Ykov91 <yakov.litvinenko.91@mail.ru>
2024-12-08 14:38:22 +0900

    Commit 3 Animal add method Speak

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Animal {
+
+public class Animal  implements AnimalSays {
+    @Override
+    public String speak() {
+        String speak = "Animal speak...";
+        return speak;
+    }
 }
